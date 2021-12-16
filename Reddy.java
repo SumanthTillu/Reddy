bb3addd6ec20dd8d7138b54ee237a29d0dc64c20
Sumanth <challasumanth0815@gmail.com>
2021-12-16 21:35:16 +0530

    first commit

---
+++ Reddy.java
@@ -0,0 +1,12 @@
+class Reddy
+{
+	public static void main(String[] args) 
+	{
+		int count=1;
+		for(char ch='A' ; ch<='Z' ;ch++)
+		{
+			System.out.println(ch+ " " +count);
+			count++;
+		}
+	}
+}
